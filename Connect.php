3a1eaffd11b8d21c2c6c7237e88fecc126542a5b
Lê Trọng Lợi <trongloikt192@gmail.com>
2013-12-21 08:40:29 +0700

    Ready

---
+++ Connect.php
@@ -0,0 +1,14 @@
+<?php
+    $host = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "dbwebphim";
+	
+	$my_connect = mysql_connect($host, $username, $password);
+	// Check connection
+	if (!$my_connect)
+	{
+		echo "Failed to connect to MySQL: " . mysql_error();
+	}
+	mysql_select_db($dbname, $my_connect);
+	mysql_query("SET character_set_results=utf8", $my_connect);
---
+++ indexz.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+		<title>TitleTile</title>
+		<meta name="description" content="" />
+		<meta name="author" content="TrongLoi" />
+
+		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
+
+		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+		<link rel="shortcut icon" href="/favicon.ico" />
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+		
+		<!-- Css -->
+		<link href="css/bootstrapv3.css" type="text/css" rel="stylesheet" media="print,screen" />
+        <link href="css/bootstrap-glyphicons.css" type="text/css" rel="stylesheet" media="print,screen" />
+		<link href="css/jquery.dataTables.css" type="text/css" rel="stylesheet" media="print,screen" />
+		<link href="css/jquery.qtip.css" type="text/css" rel="stylesheet" media="print,screen" />
+        <link href="css/header.css" type="text/css" rel="stylesheet" media="print,screen" />
+        <link href="css/body.css" type="text/css" rel="stylesheet" media="print,screen" />
+		<link href="css/footer.css" type="text/css" rel="stylesheet" media="print,screen" />
+		<link href="css/style.css" type="text/css" rel="stylesheet" media="print,screen" />
+		
+		<!-- Javascript -->
+		<script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.js" type="text/javascript"></script>
+        <script src="js/jquery.dataTables.min.js" type="text/javascript"></script>
+        <script src="js/jquery.qtip.js" type="text/javascript"></script>
+		
+	</head>
+
+	<body>
+		<div>
+			<header>
+				<h1>indexz</h1>
+			</header>
+			<nav>
+				<p>
+					<a href="/">Home</a>
+				</p>
+				<p>
+					<a href="/contact">Contact</a>
+				</p>
+			</nav>
+
+			<div>
+
+			</div>
+
+			<footer>
+				<p>
+					&copy; Copyright  by TrongLoi
+				</p>
+			</footer>
+		</div>
+	</body>
+</html>
